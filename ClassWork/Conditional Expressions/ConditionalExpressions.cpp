085758bdbbdf2776d83006acc28c5e26c8c8be74
FoPPi <mishk908@gmail.com>
2022-11-04 10:45:59 +0200

    update

--- ClassWork/Conditional Expressions/ConditionalExpressions.cpp
+++ ClassWork/Conditional Expressions/ConditionalExpressions.cpp
@@ -24,6 +24,76 @@ int main()
 
     cout << count;
 
+    // > >= < <= == != !
+
+    int n = 4;
+    bool isOdd = n % 2;
+    if (isOdd)
+        cout << "isOdd" << endl;
+        cout << n << endl;
+
+    
+    if (isOdd)
+    { 
+        cout << "isOdd" << endl;
+        cout << n << endl;
+    }
+    else{
+        cout << "even" << endl;
+    }
+    
+
+    
+
+    int a;
+    a = 4;//cin >> a;
+
+    int b;
+    b = 85; //cin >> b;
+
+    // 2. Знайти максимальне число.
+    int max = a;
+    bool res = (a > b);
+    if (a > b) 
+    { 
+        max = a;
+    }
+    cout << max;
+
+
+    n = 3;
+    switch (n)
+    {
+    case 1:
+        cout << "One";
+        break;
+    case 2:
+        cout << "Two";
+        break;
+    case 3:
+        cout << "Tree";
+        break;
+    case 4:
+        cout << "Four";
+        break;
+    
+    default:
+        cout << "Error";
+        break;
+    }
+
+    cin >> n;
+    if(n > 0){
+        cout << "positive";
+    }
+    else if (n < 0)
+    {
+        cout << "negative";
+    }
+    else{
+        cout << "zero";
+    }
+
     cout << "\n";
     system("pause");
     return 0;

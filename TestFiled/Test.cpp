6578511908a68af591a479e82e0bdd4f53e741a4
FoPPi <mishk908@gmail.com>
2023-02-02 08:04:52 +0200

    laba4

--- TestFiled/Test.cpp
+++ TestFiled/Test.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include<algorithm>
 
 using namespace std;
 
@@ -7,62 +6,7 @@ int main()
 {
     system("cls"); //system("cls");
 
-    // int n = 0;
-    // cin >> n;
-    // int max = n % 10;
-    // n /= 10; 
-    // if (n <= 2147483647)
-    // {
-    //     while (n > 0)
-    //     {
-    //         if(n % 10 > max){
-    //             max = n % 10;
-    //         }
-    //         n /= 10; 
-    //     }
-        
-    // }
-    // cout << max;
-
-    // int x1, x2;
-    // cin >> x1;
-    // cin >> x2;
-
-    // if( x1 > x2){
-    //     x1 = x1 + x2;
-    //     x2 = x1 - x2;
-    //     x1 = x1 - x2;
-    // }
-
-
-    // for (int i = x1; i <= x2; i++)
-    // {
-    //     cout << i << " ";
-    // }
-    // cout << "\n";
-
-    // while (x1 <= x2)
-    // {
-    //     cout << x1 << " ";
-    //     x1++;
-    // }
-    // cout << "\n";
-
-    // do{
-    //     cout << x1 << " ";
-    //     x1++;
-    // } while (x1 <= x2);
-    // cout << "\n";
-
-    // int n = 0, sum = 0;
-    // cin >> n;
-
-    // while (n > 0)
-    // {
-    //     sum += n % 10;
-    //     n /= 10;
-    // }
-    // cout << sum << endl;
+    
 
     cout << "\n";
     cin.ignore().get(); //system("pause");
@@ -70,3 +14,4 @@ int main()
 
     return 0;
 }
+
